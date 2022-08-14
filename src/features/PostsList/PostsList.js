9f2ab2b486c6c43958c1e9dc8248d9ffc7b564be
Leonardo Pizarro <leonardo.pizarro@mail.udp.cl>
2022-08-14 02:54:13 -0400

    deleted unused variables and imports

--- src/features/PostsList/PostsList.js
+++ src/features/PostsList/PostsList.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectSearchField } from '../Search/searchSlice';
 import Post from './Post';
-import { deletePost } from './postslistSlice';
 
 export function PostsList() {
-    const dispatch = useDispatch()
     const loadingStatus = useSelector((state) => state.postsList.requestStatus)
     const posts = useSelector((state) => state.postsList.posts)
     const searchField = useSelector(selectSearchField)
-    const handleDeletePost = (e) => console.log(e.target.value)
-    console.log('ult',searchField)
-    console.log(posts)
     const filteredPosts = posts.filter(post => {return post.name.toLowerCase().includes(searchField.toLowerCase())})
 
     if (loadingStatus === 'loading') {
